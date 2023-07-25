8157c194c94456123d917997663ca81ec80b790a
DavidLucasK <davidlucasfr70@gmail.com>
2023-07-25 18:34:01 -0300

    Toques finais em estilização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,27 +73,27 @@ export default function Home() {
               <a href="" className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
                 <img src="/Marcela.png" className='w-full' width={120} height={120} alt="Capa"/>
                 <strong className='font-semibold'>This Is Te Amo</strong>
-                <span className='text-xs text-zinc-500'>David Lucas</span>
+                <span className='text-xs text-zinc-400'>David Lucas</span>
               </a>
               <a href="" className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
                 <img src="/album1.png" className='w-full' width={120} height={120} alt="Capa"/>
-                <strong className='font-semibold'>Daily Mix 1</strong>
-                <span className='text-xs text-zinc-500'>David Lucas</span>
+                <strong className='font-semibold'>This Is Red Hot Chilli Peppers</strong>
+                <span className='text-xs text-zinc-400'>Red Hot Chilli Peppers</span>
               </a>
               <a href="" className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-                <img src="/album1.png" className='w-full' width={120} height={120} alt="Capa"/>
-                <strong className='font-semibold'>Daily Mix 1</strong>
-                <span className='text-xs text-zinc-500'>David Lucas</span>
+                <img src="/album3.png" className='w-full' width={120} height={120} alt="Capa"/>
+                <strong className='font-semibold'>This Is Switchfoot</strong>
+                <span className='text-xs text-zinc-400'>Switchfoot</span>
               </a>
               <a href="" className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-                <img src="/album1.png" className='w-full' width={120} height={120} alt="Capa"/>
-                <strong className='font-semibold'>Daily Mix 1</strong>
-                <span className='text-xs text-zinc-500'>David Lucas</span>
+                <img src="/album5.png" className='w-full' width={120} height={120} alt="Capa"/>
+                <strong className='font-semibold'>This is Taylor Swift</strong>
+                <span className='text-xs text-zinc-400'>Taylor Swift</span>
               </a>
               <a href="" className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
-                <img src="/album1.png" className='w-full' width={120} height={120} alt="Capa"/>
-                <strong className='font-semibold'>Daily Mix 1</strong>
-                <span className='text-xs text-zinc-500'>David Lucas</span>
+                <img src="/album6.png" className='w-full' width={120} height={120} alt="Capa"/>
+                <strong className='font-semibold'>This Is Oficina G3</strong>
+                <span className='text-xs text-zinc-400'>Oficina G3</span>
               </a>
             </div>
           </main>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList,Laptop2, Volume, Maximize2} from 'lucide-react'
+import { RectangleHorizontal, Heart, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList,Laptop2, Volume, Maximize2} from 'lucide-react'
 import Image from 'next/image'
 
 export function Footer() {
@@ -7,8 +7,12 @@ export function Footer() {
           <div className='flex items-center gap-3'>
             <Image src="/Ma.png" width={56} height={56} alt="Te amo Ma"/>
             <div className='flex flex-col'>
-              <strong className='font-normal'>Te amo Ma</strong>
-              <span className='text-xs text-zinc-400'>David Lucas</span>
+              <strong className='font-normal hover:underline'>Te amo Ma</strong>
+              <span className='text-xs text-zinc-400 hover:underline'>David Lucas</span>
+            </div>
+            <div className='flex items-center gap-2'>
+                <Heart size={20} className='text-zinc-100/80 hover:text-white-100/100 hover:scale-105 hover:ease-in-out duration-150'/>
+                <RectangleHorizontal size={20} className='text-zinc-100/80 hover:text-zinc-100/100' />
             </div>
           </div>
           <div className='flex flex-col items-center gap-2'>
@@ -33,13 +37,12 @@ export function Footer() {
             <Mic2 size={20} />
             <LayoutList size={20} />
             <Laptop2 size={20} />
-            <div className='flex items-center gap-2'>
+            <div className='flex items-center gap-.5'>
               <Volume size={20} />
-              <div className='h-1 rounded-full w-24 bg-zinc-600'>
-                <div className='bg-zinc-200 w-10 h-1 rounded-full'></div>
+              <div className='pai h-1 rounded-full w-24 bg-zinc-600'>
+                <div className='bg-zinc-200 w-10 h-1 rounded-full hover: pai.bg-green-600'></div>
               </div>
             </div>
-            <Maximize2 size={20} />
           </div>
         </footer>
     )
